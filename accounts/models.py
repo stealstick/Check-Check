3862e6b11b1707f73a905cc3884fbdbaabe986a1
stealstick <stillstick40@gmail.com>
2018-05-22 17:23:16 +0900

    first commit

---
+++ accounts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.utils import timezone
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+    username = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
+    email =models.CharField(max_length=100, default="")
+    is_active = models.BooleanField(default=True)
+    is_staff= models.BooleanField(default=True)
+    date_joined =models.BooleanField(default=True)
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name', 'email']
+    def __str__(self):
+        return '%s' % self.name
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
---
+++ main/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from accounts.models import User
+
+UNIT_CHOICE = (
+        ('1', '생활과 윤리의 의의 '),
+        ('2', '생명.성 윤리'),
+        ('3', '가족 윤리'),
+        ('4', '과학.생태.정보 윤리'),
+        ('5', '사회 정의'),
+        ('6', '직업윤리'),
+        ('7', '문화와 윤리'),
+        ('8', '평화와 윤리'),
+    )
+class Problem(models.Model):
+    title = models.CharField(max_length=100, help_text="문제 이름")
+    unit = models.CharField(max_length=30, help_text="단원", choices=UNIT_CHOICE)
+    ex1 = models.CharField(max_length=200, help_text="보기1")
+    ex2 = models.CharField(max_length=200, help_text="보기2")
+    ex3 = models.CharField(max_length=200, help_text="보기3")
+    ex4 = models.CharField(max_length=200, help_text="보기4")
+    ex5 = models.CharField(max_length=200, help_text="보기5")
+    answer = models.PositiveIntegerField(default=0, help_text="정답")
+    solver = models.ManyToManyField(User, related_name="solver")
+    no_solver = models.ManyToManyField(User, related_name="nosolver")
+    def __str__(self):
+        return self.title
