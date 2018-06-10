36457abf44d7f7549efd7df0da4b7888610586fa
stealstick <stillstick40@gmail.com>
2018-06-10 21:21:38 +0900

    add: Unit modle admin page

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Problem 
+from .models import Problem, Unit
 # Register your models here.
 admin.site.register(Problem)
+admin.site.register(Unit)
